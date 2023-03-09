037217d243bb5e1ba211f1aa1da5caafc3f43884
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-09 17:22:04 +0100

    TestCase & APITestCase with 'coverage'

--- my_site/tests.py
+++ my_site/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from my_site.models import Post, Category
 
-# Create your tests here.
+
+class TestCreatePost(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='django')
+        testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789')
+        test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='Post Excerpt',
+                                        content='Post Content', slug='post-title', author_id='1', status='published')
+
+    def test_my_site_content(self):
+        post = Post.postobjects.get(id=1)
+        cat = Category.objects.get(id=1)
+        author = f'{post.author}'
+        excerpt = f'{post.excerpt}'
+        title = f'{post.title}'
+        content = f'{post.content}'
+        status = f'{post.status}'
+        self.assertEqual(author, 'test_user1')
+        self.assertEqual(title, 'Post Title')
+        self.assertEqual(content, 'Post Content')
+        self.assertEqual(status, 'published')
+        self.assertEqual(str(post), 'Post Title')
+        self.assertEqual(str(cat), 'django')
--- my_site_api/tests.py
+++ my_site_api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from my_site.models import Post, Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTest(APITestCase):
+
+    def test_view_post(self):
+        url = reverse('my_site_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def create_post(self):
+        self.test_category = Category.objects.create(name='django')
+
+        self.testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789')
+
+        data = {"title": "new", "author": 1,
+                "excerpt": "new", "content": "new"}
+        url = reverse('my_site:listcreate')
+        print(url)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
