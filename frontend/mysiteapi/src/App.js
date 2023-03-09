7ebde75ff04b8ccce71f380f2d2fe555559c5c5b
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-09 21:30:19 +0100

    First API created by backend and consumed by frontend

--- frontend/mysiteapi/src/App.js
+++ frontend/mysiteapi/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+
+import React from 'react';
+
+class connectionExample extends React.Component {
+    componentDidMount() {
+        const apiUrl = 'http://127.0.0.1:8000/api/';
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+    }
+    render() {
+        return <div>Example connection</div>;
+    }
 }
 
-export default App;
+export default connectionExample;
