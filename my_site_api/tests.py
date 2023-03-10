8861d49b9734444b9033db9046a7f3a510d81082
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-10 21:10:05 +0100

    granting project/users/group/view/object level permissions & testing it

--- my_site_api/tests.py
+++ my_site_api/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from my_site.models import Post, Category
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
 class PostTest(APITestCase):
@@ -12,17 +13,51 @@ class PostTest(APITestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def create_post(self):
+    def test_create_post(self):
+        client = APIClient()
+        self.test_category = Category.objects.create(name='django')
+
+        self.testuser1 = User.objects.create_superuser(
+            username='test_user1', password='123456789')
+        client.login(username=self.testuser1.username,
+                     password='123456789')
+
+        data = {"title": "new", "author": 1,
+                "excerpt": "new", "content": "new"}
+        url = reverse('my_site_api:listcreate')
+        response = client.post(url, data, format='json')
+        # print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_update(self):
+        client = APIClient()
+
         self.test_category = Category.objects.create(name='django')
 
         self.testuser1 = User.objects.create_user(
             username='test_user1', password='123456789')
+        self.testuser2 = User.objects.create_user(
+            username='test_user2', password='123456789')
+        test_post = Post.objects.create(
+            category_id=1, title='Post Title', excerpt='Post Excerpt', content='PostContent',
+            slug='post-title', author_id=1, status='published'
+        )
 
-        data = {"title": "new", "author": 1,
-                "excerpt": "new", "content": "new"}
-        url = reverse('my_site:listcreate')
-        print(url)
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        client.login(username=self.testuser1.username,
+                     password='123456789')
+
+        url = reverse('my_site_api:detailcreate', kwargs={'pk': 1})
+
+        response = client.put(
+            url, {
+                # "id": 1,
+                "title": "new",
+                "author": 1,
+                "excerpt": "new",
+                "content": "new",
+                "status": "published"
+            }, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
