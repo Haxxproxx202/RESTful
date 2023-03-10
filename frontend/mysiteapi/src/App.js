c501ccac9f0a2ca3572be95e33cb73a6df4e9400
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-10 18:22:55 +0100

    simple MUI implemented + first tests added

--- frontend/mysiteapi/src/App.js
+++ frontend/mysiteapi/src/App.js
@@ -1,17 +1,82 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Posts from './components/Posts';
+import PostLoadingComponent from './components/PostLoading';
 
 
-import React from 'react';
+const App = () => {
+    const PostLoading = PostLoadingComponent(Posts);
+    const [appState, setAppState] = useState({
+        loading: false,
+        posts: null,
+    });
+    const [list, setList] = useState([1, 2, 3, 4, 5]);
 
-class connectionExample extends React.Component {
-    componentDidMount() {
+    useEffect(() => {
+        setAppState({ loading: true});
         const apiUrl = 'http://127.0.0.1:8000/api/';
         fetch(apiUrl)
             .then((response) => response.json())
-            .then((data) => console.log(data))
-    }
-    render() {
-        return <div>Example connection</div>;
-    }
+            .then((data) => {
+                setAppState(prevState => {
+                    return {
+                        loading: false,
+                        posts: data
+                    }
+                })
+            })
+            .catch((err) => console.log(err))
+    }, [setAppState]);
+    return (
+        <div className="App">
+            <h1>Latest Posts</h1>
+            {/*{*/}
+            {/*    appState.posts.map((el, id) => (*/}
+            {/*        <PostLoading key={id} isLoading={appState.loading} />*/}
+            {/*    ))*/}
+            {/*}*/}
+            {/*<PostLoading isLoading={appState.loading} posts={appState.posts} />*/}
+            <PostLoading isLoading={appState.loading} posts={appState.posts} />
+        </div>
+    );
 }
 
-export default connectionExample;
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+//
+// class connectionExample extends React.Component {
+//     componentDidMount() {
+//         const apiUrl = 'http://127.0.0.1:8000/api/';
+//         fetch(apiUrl)
+//             .then((response) => response.json())
+//             .then((data) => console.log(data))
+//     }
+//     render() {
+//         return <div>Example connection</div>;
+//     }
+// }
+//
+// export default connectionExample;
