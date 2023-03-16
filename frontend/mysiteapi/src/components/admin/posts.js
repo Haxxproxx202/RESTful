66bcaab09e0dedc1dac63f964c2d9281efaa237f
Haxxproxx202 <pjanecki88@gmail.com>
2023-03-16 22:19:01 +0100

    Handling CRUD requests - part2

--- frontend/mysiteapi/src/components/admin/posts.js
+++ frontend/mysiteapi/src/components/admin/posts.js
@@ -11,6 +11,12 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import EditIcon from '@mui/icons-material';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
+import Link from "@mui/joy/Link";
+import Button from "@mui/material/Button";
+import {Fragment} from "react";
 
 
 
@@ -41,31 +47,46 @@ export default function CustomizedTables(props) {
 
     if(!posts || posts.length === 0) return <p>Can not find any posts, sorry for that</p>
     return (
-        <TableContainer component={Paper} >
-            <Table sx={{ minWidth: 700 }} aria-label="customized table" >
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Title</StyledTableCell>
-                        <StyledTableCell align="right" >ID</StyledTableCell>
-                        <StyledTableCell align="right">Author</StyledTableCell>
-                        <StyledTableCell align="right">Category</StyledTableCell>
-                        <StyledTableCell align="right">Content</StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {posts.map((el) => (
-                        <StyledTableRow key={el.id} style={{margin: "80px"}}>
-                            <StyledTableCell component="th" scope="row" style={{margin: "80px"}}>
-                                {el.title}
-                            </StyledTableCell>
-                            <StyledTableCell align="right">{el.id}</StyledTableCell>
-                            <StyledTableCell align="right">{el.author}</StyledTableCell>
-                            <StyledTableCell align="right">{el.category}</StyledTableCell>
-                            <StyledTableCell align="right">{el.content.substring(0, 20)}</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Fragment>
+            <TableContainer component={Paper} >
+                <Table sx={{ minWidth: 700 }} aria-label="customized table" >
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Title</StyledTableCell>
+                            <StyledTableCell align="right" >ID</StyledTableCell>
+                            <StyledTableCell align="right">Author</StyledTableCell>
+                            <StyledTableCell align="right">Category</StyledTableCell>
+                            <StyledTableCell align="right">Content</StyledTableCell>
+                            <StyledTableCell align="right">Actions</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {posts.map((el) => (
+                            <StyledTableRow key={el.id} style={{margin: "80px"}}>
+                                <StyledTableCell component="th" scope="el" style={{margin: "80px"}}>
+                                    {el.title}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{el.id}</StyledTableCell>
+                                <StyledTableCell align="right">{el.author}</StyledTableCell>
+                                <StyledTableCell align="right">{el.category}</StyledTableCell>
+                                <StyledTableCell align="right">{el.content.substring(0, 20)}</StyledTableCell>
+                                <StyledTableCell align="right">
+                                    <Link href={'/admin/edit/' + el.id}>
+                                        <EditRoundedIcon />
+                                    </Link>
+                                    <Link href={'/admin/delete' + el.id} style={{color: "red"}}>
+                                        <DeleteForeverRoundedIcon />
+                                    </Link>
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+
+                    </TableBody>
+
+                </Table>
+            </TableContainer>
+            <Button variant="contained" href={'/admin/create'} style={{float: "right"}}>New Post</Button>
+        </Fragment>
+
     );
 }
